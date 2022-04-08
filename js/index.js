68c596c91391bc391932093984d483bbd459c9a7
majaliju <majalijunyc@gmail.com>
2022-04-08 18:28:26 -0400

    added some comments for clarity

--- js/index.js
+++ js/index.js
@@ -7,12 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
 // define our activities database so we only fetch once
 let database 
 
-// define some variables global for easier use later
+// define some variables globally for easier use later
 const mainBody = document.getElementById("main-body")
 const boredButton = document.getElementById('bored-button')
 
 
-// our first function, stores the data in a variable 
+// our first function, stores the data in a variable
+// then commences to loadActivity and start our page
 function getActivities() { 
   fetch("http://www.boredapi.com/api/activity/")
   .then(response => response.json())
@@ -41,7 +42,7 @@ function wipeMainBody (){
 function showNewActivity(){
     const h1 = document.createElement('h1')
     const h2 = document.createElement('h2')
-    const h3 = document.createElement('h3')
+    // const h3 = document.createElement('h3')
     mainBody.append(h1)
     mainBody.append(h2)
     h1.innerText = database.activity
