0549e6ee085fc26c278dd20ceada20bc0d972caa
majaliju <majalijunyc@gmail.com>
2022-04-08 17:36:08 -0400

    cleaned up functions

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ let database
 
 // define some variables global for easier use later
 const mainBody = document.getElementById("main-body")
-
+const boredButton = document.getElementById('bored-button')
 
 
 // our first function, stores the data in a variable 
@@ -25,24 +25,10 @@ function getActivities() {
 
 // our button action, that loads an activity for the user
 function loadActivity() {
-  const boredButton = document.getElementById('bored-button')
-
   boredButton.addEventListener('click', (e) => {
     wipeMainBody()
-    const h1 = document.createElement('h1')
-    const h2 = document.createElement('h2')
-    const h3 = document.createElement('h3')
-    mainBody.append(h1)
-    mainBody.append(h2)
-    // mainBody.append(h3)
-    h1.innerText = database.activity
-    h2.innerText = `For the ${database.type} lovers`
-    // h3.innerText = 
-
+    showNewActivity();
   })
-
-
-  // change boredButton's innerText to "I'm still bored!" for reset
 }
     
 // this function clears our mainBody div element
@@ -51,3 +37,30 @@ function wipeMainBody (){
   mainBody.innerHTML = ""
 }
 
+// shows a newActivity on the page
+function showNewActivity(){
+    const h1 = document.createElement('h1')
+    const h2 = document.createElement('h2')
+    const h3 = document.createElement('h3')
+    mainBody.append(h1)
+    mainBody.append(h2)
+    h1.innerText = database.activity
+    h2.innerText = `For the ${database.type} lovers`
+    mainBody.append(boredButton)
+    boredButton.innerText = "I'm still bored!"
+    loadActivity()
+    
+    
+}
+
+
+// function loadNewActivity(){
+//   const stillBoredButton = document.createElement('button')
+//   mainBody.append(stillBoredButton)
+//   stillBoredButton.innerText = "I'm still bored!"
+
+//   stillBoredButton.addEventListener('click', (e)=>{
+//     wipeMainBody()
+//     showNewActivity();
+//   })
+// }
