c1db62ecf7aaaa67a3f23503de33635e27367a76
majaliju <majalijunyc@gmail.com>
2022-04-08 16:50:58 -0400

    updated HTML, js for functions

--- js/index.js
+++ js/index.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
   getActivities()
 })
 
+// define our activities database so we only fetch once
 let database 
 
 function getActivities() { 
@@ -11,11 +12,20 @@ function getActivities() {
   .then(response => response.json())
   .then(data => {
     database = data
-    console.log("activities: ", database)
-    console.log("show activity: ", database.activity)
+    // console.log("activities: ", database)
+    // console.log("show activity: ", database.activity)
   })
+  loadActivity()
+
 }
   
+function loadActivity() {
+  const boredButton = document.getElementById('bored-button')
+
+  addEventListener('click', (e) => {
+    console.log(e.target)
+  })
+}
     
 
 
