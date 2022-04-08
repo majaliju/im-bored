c3d4210df68ece85835caf42712171eb5e935f0e
majaliju <majalijunyc@gmail.com>
2022-04-08 17:22:38 -0400

    added the h1 and h2 events for the new page

--- js/index.js
+++ js/index.js
@@ -7,23 +7,37 @@ document.addEventListener('DOMContentLoaded', () => {
 // define our activities database so we only fetch once
 let database 
 
+// define some variables global for easier use later
+const mainBody = document.getElementById("main-body")
+
+
+
+// our first function, stores the data in a variable 
 function getActivities() { 
   fetch("http://www.boredapi.com/api/activity/")
   .then(response => response.json())
   .then(data => {
     database = data
-    // console.log("activities: ", database)
-    // console.log("show activity: ", database.activity)
   })
   loadActivity()
 
 }
-  
+
+// our button action, that loads an activity for the user
 function loadActivity() {
   const boredButton = document.getElementById('bored-button')
 
   boredButton.addEventListener('click', (e) => {
-    console.log(e.target)
+    wipeMainBody()
+    const h1 = document.createElement('h1')
+    const h2 = document.createElement('h2')
+    const h3 = document.createElement('h3')
+    mainBody.append(h1)
+    mainBody.append(h2)
+    // mainBody.append(h3)
+    h1.innerText = database.activity
+    h2.innerText = `For the ${database.type} lovers`
+    // h3.innerText = 
 
   })
 
@@ -31,7 +45,9 @@ function loadActivity() {
   // change boredButton's innerText to "I'm still bored!" for reset
 }
     
+// this function clears our mainBody div element
 function wipeMainBody (){
-  // wipe the mainBody div 
+  const mainBody = document.getElementById("main-body")
+  mainBody.innerHTML = ""
 }
 
