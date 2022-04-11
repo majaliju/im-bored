8eda84cd3d8d5a7047673e3d91687cbbd9fbca20
majaliju <majalijunyc@gmail.com>
2022-04-11 15:30:59 -0400

    git commit -m

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 
 document.addEventListener('DOMContentLoaded', () => {
-  getActivities()
+  
 })
 
-// define our activities database so we only fetch once
-let database 
 
 // define some variables globally for easier use later
 const mainBody = document.getElementById("main-body")
@@ -17,19 +15,19 @@ function getActivities() {
   fetch("http://www.boredapi.com/api/activity/")
   .then(response => response.json())
   .then(data => {
-    database = data
+    console.log("data: ", data)
+    showNewActivity(data)
   })
-  loadActivity()
 
 }
 
 // our button action, that loads an activity for the user
-function loadActivity() {
+
   boredButton.addEventListener('click', (e) => {
     wipeMainBody()
-    showNewActivity();
+    getActivities()
+    // showNewActivity();
   })
-}
     
 // this function clears our mainBody div element
 function wipeMainBody (){
@@ -38,29 +36,19 @@ function wipeMainBody (){
 }
 
 // shows a newActivity on the page
-function showNewActivity(){
+function showNewActivity(database){
     const h1 = document.createElement('h1')
     const h2 = document.createElement('h2')
-    // const h3 = document.createElement('h3')
     mainBody.append(h1)
     mainBody.append(h2)
     h1.innerText = database.activity
     h2.innerText = `For the ${database.type} lovers`
     mainBody.append(boredButton)
     boredButton.innerText = "I'm still bored!"
-    loadActivity()
     
+    // mainBody --> use the new event, no point in clearing
     
 }
 
-
-// function loadNewActivity(){
-//   const stillBoredButton = document.createElement('button')
-//   mainBody.append(stillBoredButton)
-//   stillBoredButton.innerText = "I'm still bored!"
-
-//   stillBoredButton.addEventListener('click', (e)=>{
-//     wipeMainBody()
-//     showNewActivity();
-//   })
-// }
+// store a favorites list
+// add a spacebar event
